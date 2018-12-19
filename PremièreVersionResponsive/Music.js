4efca1a12b557ab2bac877f3d058e7d9feba4f91
Smartiiz <t.lefloch53@gmail.com>
2018-12-19 16:35:35 +0100

    dernière modif

--- PremièreVersionResponsive/Music.js
+++ PremièreVersionResponsive/Music.js
@@ -1,20 +1,19 @@
 class Music {
 
-    constructor(jsonFile){
-        let data = JSON.parse(jsonFile);
-        this.idPlage = data.idPlage;
-        this.titre = data.titre;
-        this.duree = data.duree;
-        this.nbLike = data.nbLike;
-        this.nbCommentaire = data.nbCommentaire;
-        this.nbEcoute = data.nbEcoute;
-        this.cheminPic = data.cheminPic;
-        this.nomAuteur = data.nomAuteur;
-        this.anneePlage = data.anneePlage;
-        this.pochette = data.pochette;
-        this.cheminMP3 = data.cheminMP3;
-    }
-
+  constructor(jsonFile) {
+    let data = JSON.parse(jsonFile);
+    this.idPlage = data.idPlage;
+    this.titre = data.titre;
+    this.duree = data.duree;
+    this.nbJaime = data.nbJaime;
+    this.nbCommentaire = data.nbCommentaire;
+    this.nbEcoute = data.nbEcoute;
+    this.pic = data.pic;
+    this.nomAuteur = data.nomAuteur;
+    this.anneeMusique = data.anneeMusique;
+    this.cheminPochette = data.cheminPochette;
+    this.cheminMP3 = data.cheminMP3;
+  }
 }
 
 exports.Music = Music.prototype;
