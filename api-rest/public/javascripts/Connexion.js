19433e651ce7f026e9f1bc41ab7679ef92884049
naka <chapey.thierry@gmail.com>
2019-01-18 15:33:26 +0100

    Js Serveur
    Ajout des cas d'erreur et de la vérification des extensions .

--- api-rest/public/javascripts/Connexion.js
+++ api-rest/public/javascripts/Connexion.js
@@ -9,11 +9,9 @@ function err(callback) {
 Connexion.prototype.requestGet = function (url, callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        //     if (this.readyState == 4 && this.status == 200) {
 
         err(callback);
     };
-    //xhttp.setRequestHeader("Access-Control-Allow-Origin","");
     xhttp.open("GET", url, true);
     xhttp.send();
 };
@@ -51,46 +49,23 @@ Connexion.prototype.getMusiqueById = function (id, callback) {
 };
 
 Connexion.prototype.getMusiqueByTitre = function (title, callback) {
-    this.requestGet("http://localhost:3000/music/find/music/title/" + title, callback);
-};
-/*
-Connexion.prototype.getPlaylistById = function(id, callback){
-    this.requestGet("http://localhost:3000/music/find/playlist/id/" + id, callback);
+    this.requestGet("http://localhost:3000/musique/find/music/title/" + title, callback);
 };
 
-Connexion.prototype.getPlaylistByName = function(name, callback){
-    this.requestGet("http://localhost:3000/music/find/playlist/name/" + name, callback);
-};
 
-*/
-/**             PUT            */ //TODO
+/**             PUT            */
+
 Connexion.prototype.addLike = function (idMusic, callback) {
-    this.PutRequest("http://localhost:3000/music/maj/like/" + idMusic, null, callback);
+    this.PutRequest("http://localhost:3000/musique/updateAdd/like/" + idMusic, null, callback);
 };
 Connexion.prototype.removeLike = function (idMusic, callback) {
-    this.PutRequest("http://localhost:3000/music/maj/removeLike/" + idMusic, null, callback);
+    this.PutRequest("http://localhost:3000/musique/updateDel/like/" + idMusic, null, callback);
 };
-Connexion.prototype.addNumberOfView = function (idMusic, callback) {
-    this.PutRequest("http://localhost:3000/music/maj/views/" + idMusic, null, callback);
+Connexion.prototype.addViewNumber = function (idMusic, callback) {
+    this.PutRequest("http://localhost:3000/musique/updateAdd/view/" + idMusic, null, callback);
 };
 
 Connexion.prototype.addNumberOfShare = function (idMusic, callback) {
-    this.PutRequest("http://localhost:3000/music/maj/share/" + idMusic, null, callback);
+    this.PutRequest("http://localhost:3000/musique/updateRem/view/" + idMusic, null, callback);
 };
-Connexion.prototype.addMusicToPlaylist = function (idPlaylist, idMusic, callback){
-    this.requestPut("http://localhost:3000/music/maj/playlist/add/" + idPlaylist, JSON.parse('{"idMusic" : '+idMusic+'}'), callback);
-};
-
-Connexion.prototype.removeMusicToPlaylist = function (idPlaylist, idMusic, callback){
-    this.requestPut("http://localhost:3000/music/maj/playlist/remove/" + idPlaylist, JSON.parse('{"idMusic" : '+idMusic+'}'), callback);
-
-};
-
-/**         Common part          */
-
-/**
- * Allow to get the id (if exist) of the music put in the url
- * @returns {number}
- */
-
 var Connexion = new Connexion();
--- api-rest/routes/music-rest.js
+++ api-rest/routes/music-rest.js
@@ -23,74 +23,90 @@ myRouter.route('/')
         let mp3 = req.files.music;
         let cover = req.files.cover;
 
-        let stringJson="";
-        //Modification des nom des fichiers pour remplacer les espace par des tirets
-        let newNameSong = req.files.music.name.replace(/\s+/g, '-').toLowerCase();
-        let newNameSongNoExt = req.body.name.replace(/\s+/g, '-').toLowerCase();
-        let newNameCover = req.files.cover.name.replace(/\s+/g, '-').toLowerCase();
 
-        //Chemin d'entrée et de sortie pour la commande audioWaveform
-        let path = "../api-rest/public/songs/" + newNameSong;
-        let dest = "../api-rest/public/songData/" + newNameSongNoExt + ".json";
-        mp3.mv(path, function (err) {
-            if (err)
-                return res.status(500).send(err);
-            // recuperation des metadata
-            muisqueMeta.parseFile(path, {native: true})
-                .then(metadata => {
-                    duree = metadata.format.duration;
-                    creatJson(dest,readJson);
+        let regex = /(?:\.([^.]+))?$/;
+        let verification1 = regex.exec(req.files.music.name)[1];
+        let verification2 = regex.exec(req.files.cover.name)[1];
+
+        if (verification1 ==='mp3' && verification2 ==='jpg') {
+
+
+            //Modification des nom des fichiers pour remplacer les espace par des tirets
+            let songwithoutSpace = req.files.music.name.replace(/\s+/g, '-').toLowerCase();
+            let newNameSongNoExtNoSPace = req.body.name.replace(/\s+/g, '-').toLowerCase();
+
+            //Remove des éléments qui sont dans les parenthèses.
+            var newNameSong = songwithoutSpace.replace(/\(.[^(]*\)/g, '');
+            let newNameSongNoExt = newNameSongNoExtNoSPace.replace(/\(.[^(]*\)/g, '');
+
+            let newNameCover = req.files.cover.name.replace(/\s+/g, '-').toLowerCase();
+            //Chemin d'entrée et de sortie pour la commande audioWaveform
+            let path = "../api-rest/public/songs/" + newNameSong;
+            let dest = "../api-rest/public/songData/" + newNameSongNoExt + ".json";
+            mp3.mv(path, function (err) {
+                if (err)
+                    return res.status(500).send(err);
+                // recuperation des metadata
+                muisqueMeta.parseFile(path, {native: true})
+                    .then(metadata => {
+                        duree = metadata.format.duration;
+                        creatJson(dest, readJson);
+
+                    })
+                    .catch(err => {
+                        console.log(err.message);
+                    });
+            });
+            cover.mv('../api-rest/public/cover/' + newNameCover, function (err) {
+                if (err)
+                    return res.status(500).send(err);
+            });
+
+            function creatJson(dest, callback) {
+                shell.exec('audiowaveform -i ' + path + ' -b 8 -z 44100 -o ' + dest);
+                callback(dest, envoi);
+            }
+
+            function readJson(dest, callback) {
+                let donnee = fs.readFileSync(dest, 'utf8');
+                words = JSON.parse(donnee);
+                callback(dest, delJSON);
+            }
+
+            //destruction du fichier JSON
+            function delJSON() {
+                shell.exec('rm ' + dest);
+
+            }
+
+            //Fonction d'envoi dans la base de données.
+            function envoi(dest, callback) {
+                let request = {
+                    idPlage: 9,
+                    titre: req.body.name,
+                    duree: duree,
+                    nbLike: 0,
+                    nbCommentaire: 0,
+                    nbEcoute: 0,
+                    peaks: words.data,
+                    nomAuteur: req.body.author,
+                    anneePlage: req.body.date,
+                    cheminPochette: '/SoundWave/api-rest/public/cover/' + newNameCover,
+                    cheminMP3: '/SoundWave/api-rest/public/songs/' + newNameSong
+                };
+                musique.insertOne(request, function (err, result) {
+                    if (err) next(err);
+                    else {
+                        res.render('ajoutSucces');
+                        console.log("Le Fichier JSON viens d'être retiré.");
+                        callback(delJSON);
+                    }
 
                 })
-                .catch(err => {
-                    console.log(err.message);
-                });
-        });
-        cover.mv('../api-rest/public/cover/' + newNameCover, function (err) {
-            if (err)
-                return res.status(500).send(err);
-        });
-
-        function creatJson(dest,callback) {
-            shell.exec('audiowaveform -i ' + path + ' -b 8 -z 44100 -o ' + dest);
-            callback(dest,envoi);
+            }
         }
-
-        function readJson(dest,callback) {
-               let donnee = fs.readFileSync(dest, 'utf8');
-               words=JSON.parse(donnee);
-               callback(dest,delJSON);
-        }
-        //destruction du fichier JSON
-        function delJSON() {
-            shell.exec('rm ' + dest);
-
-        }
-
-        //Fonction d'envoi dans la base de données.
-        function envoi(dest,callback) {
-            let request = {
-                idPlage : 9,
-                titre: req.body.name,
-                duree: duree,
-                nbLike: 0,
-                nbCommentaire: 0,
-                nbEcoute: 0,
-                peaks: words.data,
-                nomAuteur: req.body.author,
-                anneePlage: req.body.date,
-                cheminPochette: '/SoundWave/api-rest/public/cover/' + newNameCover,
-                cheminMP3: '/SoundWave/api-rest/public/cover/' + newNameSong
-            };
-            musique.insertOne(request, function (err, result) {
-                if (err) next(err);
-                else {
-                    res.render('ajoutSucces');
-                    console.log("Le Fichier JSON viens d'être retiré.");
-                    callback(delJSON);
-                }
-
-            })
+        else {
+            return res.send("Les fichiers fournis ne sont pas au bon format.");
         }
     });
 
@@ -135,9 +151,45 @@ myRouter.route('/titre/:titre')
             res.json(result);
         });
     });
+myRouter.route('/updateAdd/like/')
+    .put(function (req,res) {
+        musique.updateOne({idPlage: req.params.id}, {$inc:{nbLike : 1}}, function(err, status) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(status);
+        });
 
+    });
+myRouter.route('/updateDel/like/')
+    .put(function (req,res) {
+        musique.updateOne({id: req.params.id}, {$inc:{nbLike : -1}}, function(err, status) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(status);
+        });
 
+    });
+myRouter.route('/updateAdd/view/')
+    .put(function (req,res) {
+        musique.updateOne({id: req.params.id}, {$inc:{nbEcoute : 1}}, function(err, status) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(status);
+        });
 
+    });
+myRouter.route('/updateRem/view/')
+    .put(function (req,res) {
+        musique.updateOne({id: req.params.id}, {$inc:{nbEcoute : -1}}, function(err, status) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(status);
+        });
 
+    });
 
 module.exports = myRouter;
