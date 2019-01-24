5584fce9f7df9c6035d08a5d09719326181dd00b
Thomas Le Floch <t.lefloch53@gmail.com>
2019-01-24 09:27:12 +0100

    waveform hover encours, reglages de certains bugs

--- api-rest/public/javascripts/sound-manager.js
+++ api-rest/public/javascripts/sound-manager.js
@@ -1,30 +1,32 @@
 soundManager.url = '/javascripts/soundmanager/swf/';
-soundManager.onready(function() {
+soundManager.onready(function () {
     soundManager.createSound({
         id: song.smId,
+        waitForWindowLoad: true,
         url: song.cheminMP3,
         onpause: function () {
-            util.addClass(player.pauseButton ,'play');
-            util.removeClass(player.pauseButton,'pause');
+            util.addClass(player.pauseButton, 'play');
+            util.removeClass(player.pauseButton, 'pause');
         },
-        onresume: function() {
-            util.addClass(player.pauseButton ,'pause');
-            util.removeClass(player.pauseButton,'play');
+        onresume: function () {
+            util.addClass(player.pauseButton, 'pause');
+            util.removeClass(player.pauseButton, 'play');
         },
-        onplay: function() {
-            util.addClass(player.pauseButton ,'pause');
-            util.removeClass(player.pauseButton,'play');
+        onplay: function () {
+            util.addClass(player.pauseButton, 'pause');
+            util.removeClass(player.pauseButton, 'play');
+            player.waveform.reset();
+            player.waveform.selectFirstRect();
+        },
+        onfinish: function () {
+            util.addClass(player.pauseButton, 'play');
+            util.removeClass(player.pauseButton, 'pause');
             player.waveform.reset();
         },
-        onfinish: function() {
-            util.addClass(player.pauseButton ,'play');
-            util.removeClass(player.pauseButton,'pause');
-            player.waveform.reset();
-        },
-        whileplaying: function() {
-            let rect = util.searchRect(util.getXFromTime(this.position));
-            util.addClassSvg(rect, "passed");
-            player.setDuree(util.SecToMin(Math.round(this.position/1000)));
+        whileplaying: function () {
+            let rect = player.waveform.searchRectFromX(util.getXFromTime(this.position));
+            player.waveform.setCurrentPeak(rect);
+            player.setDuree(util.SecToMin(Math.round(this.position / 1000)));
         }
     });
     soundManager.play(song.smId);
